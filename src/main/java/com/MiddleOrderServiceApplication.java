113729aa9484338f3d9a685c71246618e2b5e8c3
Yogi Erka Julyansa Putra <clouderka@gmail.com>
2026-07-19 23:03:49 +0700

    Create MiddleOrderServiceApplication.java

---
+++ src/main/java/com/MiddleOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codebase.middle_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddleOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddleOrderServiceApplication.class, args);
+	}
+
+}
