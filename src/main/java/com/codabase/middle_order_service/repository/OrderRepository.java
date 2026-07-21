e93f82fc2743d1274c33109957e67d7823e5dd94
Yogi Erka Julyansa Putra <clouderka@gmail.com>
2026-07-21 10:21:27 +0000

    Add native query

--- src/main/java/com/codabase/middle_order_service/repository/OrderRepository.java
+++ src/main/java/com/codabase/middle_order_service/repository/OrderRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query(value = "SELECT * FROM orders WHERE product_code = :productCode", nativeQuery = true)
+    List<Order> findByProductCodeNative(@Param("productCode") String productCode);
 }
